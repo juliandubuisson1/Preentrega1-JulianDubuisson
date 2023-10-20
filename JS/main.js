de780f23d42249e420987b50288e5c97193b6ac9
Julian Dubuisson <juliandubuisson1@gmail.com>
2023-10-20 13:21:01 -0300

    eventos del boton para comprar entrenamientos

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,4 @@
-for (let i = 0; i < 1; i++){
+/* for (let i = 0; i < 1; i++){
     alert('¿Queres saber tu Indice de Masa Corporal (IMC)?')
 }
 
@@ -41,6 +41,8 @@ if(!isNaN(numeroFloat) && numeroFloat !== Math.floor(altura)){
 }
 }
 
+//CALCULO DE IMC
+
 imc= (peso / (altura * altura)).toFixed(2);
 alert("Tu IMC es: " + imc);
 
@@ -52,24 +54,34 @@ if(imc < 18.5){
     alert('Usted tiene sobrepeso');
 }else{
     alert('Usted tiene obesidad')
-} 
+}
+*/
 
 
-const entrenamientos = [
+//ARRAYS DE ENTRENAMIENTOS
+const entrenamientos= [
     {
         id:0, 
         nombre: "Báisco", 
         precio: 300,
+        thumbnail: 'https://images.unsplash.com/photo-1534258936925-c58bed479fcb?auto=format&fit=crop&q=80&w=1931&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
 
     {
         id:1, 
         nombre: "Intermedio", 
         precio: 500,
+        thumbnail:'https://images.unsplash.com/photo-1536922246289-88c42f957773?auto=format&fit=crop&q=80&w=2104&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    
+    {
+        id: 2, 
+        nombre: "Avanzado", 
+        precio : 700,
+        thumbnail:'https://images.unsplash.com/photo-1591258370814-01609b341790?auto=format&fit=crop&q=80&w=1974&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
 ];
 
-entrenamientos.push({id: 2, nombre: "Avanzado", precio : 700});
 
 const mostrarEntrenamientos = () => {
     let mensaje =  'Entrenamientos: \n';
@@ -77,9 +89,51 @@ const mostrarEntrenamientos = () => {
         mensaje += `Entrenamiento: ${entrenamiento.nombre} \nPrecio: ${entrenamiento.precio} \n ----------------------- \n` 
     })
     alert(mensaje);
-}
+};
 
 
 mostrarEntrenamientos(); 
 
-alert ('Vení a entrenar a Fit-Coach')
+alert ('Vení a entrenar a Fit-Coach') 
+
+
+//MUESTRA DE LOS ENTRENAMIENTOS EN EL HTML
+
+let contenedor = document.createElement("div");
+
+
+for ( const entrenamiento of entrenamientos) {
+    contenedor.innerHTML += `
+    <article class= "box-container" id=${entrenamiento.id}>
+        <img src= ${entrenamiento.thumbnail} alt="imagen sobre ${entrenamiento.nombre}> 
+        <div class="box-container-body">
+            <h2>${entrenamiento.nombre}</h2>
+            <p>$${entrenamiento.precio}</p>
+            <button class="comprar-boton" data-id=${entrenamiento.id}>Comprar</button>
+        </div>
+    `
+}
+
+document.body.append(contenedor);
+console.log(contenedor);
+
+
+//EVENTOS DEL BOTON COMPRAR ENTRENAMIENTOS
+
+const botonesComprar = document.querySelectorAll('.comprar-boton');
+
+function Compra() {
+    // Tu código para manejar la compra aquí
+    // Puedes acceder a los datos del entrenamiento relacionado usando el DOM
+    const contenedor = this.closest('.box-container');
+    const nombreEntrenamiento = contenedor.querySelector('h2').textContent;
+    const precioEntrenamiento = contenedor.querySelector('p').textContent;
+    
+    // Ejemplo de lo que puedes hacer con los datos
+    console.log(`Compraste ${nombreEntrenamiento} por ${precioEntrenamiento}`);
+}
+
+// Agrega el evento "click" a cada botón
+botonesComprar.forEach(boton => {
+    boton.addEventListener('click', Compra);
+});
